ec2a5da5e329318c1d15ef6dd22b047822a0b44d
Britta Velten <bvelten@dhcp-bvelten-17979.embl.de>
2018-09-14 15:33:12 +0200

    added new suppl. figures

--- plot_utils.R
+++ plot_utils.R
@@ -114,6 +114,8 @@ plotCombiResponseCurves <- function(dfBCmuts, drugs2plot, gene = "TP53", round2=
 # Function to plot comparison of two combination compunds across all base drugs
 plotComparsionCDrugs <- function(df, CDrugAbrv.x, CDrugAbrv.y, range = c(0,1.4), type=c("scatter_joint", "scatter_factBDrug", "boxplot_joint")){
 
+  type <- match.arg(type)
+  stopifnot(type %in% c("scatter_joint", "scatter_factBDrug", "boxplot_joint"))
   df.x <- filter(df, CDrugAbrv==CDrugAbrv.x)
   df.y <- filter(df, CDrugAbrv==CDrugAbrv.y)
 
@@ -529,7 +531,7 @@ plotWaterfallCI <-  function(df, drC , drB, CI_type = c("Bliss", "hsa", "SI"),
     geom_bar(stat="identity") + 
     geom_errorbar(aes(ymin = CI - CIse, ymax = CI + CIse), width = 0.3)+
     geom_hline(yintercept = ifelse(CI_type =="SI",0,1), lty = "dashed") +
-    theme_bw()  + xlab(paste0("Patient sample")) +
+    theme_bw(base_size = 15)  + xlab(paste0("Patient sample")) +
     ylab(paste0("Combination index (", CI_type, ")")) + 
     scale_fill_manual(values = patcol) +
     theme(axis.ticks.x = element_blank(),
@@ -541,11 +543,11 @@ plotWaterfallCI <-  function(df, drC , drB, CI_type = c("Bliss", "hsa", "SI"),
     gg <- gg + ylim(c(min(df4plot$CI - df4plot$CIse), max(df4plot$CI + df4plot$CIse) +0.04))
     if("IGHV" %in% annotate){
     gg <- gg + geom_text(aes(y = max(df4plot$CI + df4plot$CIse) +0.01, label = ifelse(IGHV==1, "*", "")), size=5)
-    gg <- gg + geom_text(x=nrow(df4plot)-10, y= min(df4plot$CI), label = "* M-CLL", col="black", size=5)
+    gg <- gg + geom_text(x=nrow(df4plot)-10, y= min(df4plot$CI), label = "* M-CLL", col="red", size=5)
     }
     if("TP53" %in% annotate){
     gg <- gg + geom_text(aes(y = max(df4plot$CI + df4plot$CIse) +0.03, label = ifelse(TP53==1, "*", "")), col="red", size=5)
-    gg <- gg + geom_text(x=nrow(df4plot)-3, y= min(df4plot$CI), label = "* TP53 mut", col="red", size=5)
+    gg <- gg + geom_text(x=nrow(df4plot)-3, y= min(df4plot$CI), label = "* TP53 mut", col="orange", size=5)
     }
   }
   
@@ -630,6 +632,7 @@ plotTiles10x10 <- function(df, drB, pat, type = c("tile", "contour")){
     }  
 
 plotCITiles <- function(df, CItype){
+  dfres <- data.frame() # replace by lapply, bind_rows
   for(dr in unique(df$BaseDrugName)){
     if(dr!="DM"){
       print(dr)
@@ -668,295 +671,40 @@ plotCITiles <- function(df, CItype){
                 plot.title = element_text(colour =  "black"))
         print(gg)
         # myPlotSynergy(synergy.score, type = "2D", save.file = TRUE, file.name = paste0(figdir,dr, pat,".pdf"), pat=pat)
+        dfres <- rbind(dfres,cbind(df_score, PatientID = pat, BDrugName = dr, CDrugName = "Ibrutinib"))
       }
     }
   }
+  return(dfres)
 }
-# adapted from SynergyFinder
-myPlotSynergy <- function (data, type = "2D", save.file = FALSE, len = 3, pair.index = NULL, 
-          legend.start = NULL, legend.end = NULL, row.range = NULL, 
-          col.range = NULL, file.name = NULL, pat = "") {
+
+
+plotSummaryLoewe <- function(dr, summarize_by = "median") {
+  dfLoewe_dr<- filter(dfLoewe, BDrugName == dr) %>% group_by(concB, concC) %>% summarize(sumScore = switch(summarize_by,
+                                                                                                           mean  = mean(score),
+                                                                                                           median  = median(score)))
   
-  attach(loadNamespace("synergyfinder"), name = "synergyfinder_all")
+  gg_medScore <- ggplot(dfLoewe_dr, aes(x = factor(round(concB*1000,2)), y=factor(round(concC*1000,2)), fill = sumScore)) +
+    geom_tile() +
+    ylab("Ibrutinib (nM)") + xlab(paste(dr, "(nM)")) +
+    scale_fill_gradient2(low= "blue", high="red", mid="white", midpoint = 0) +
+    theme_bw(base_size = 16) + coord_fixed() +
+    theme(axis.text.x = element_text(angle=90, vjust=1, hjust=1),
+          plot.title = element_text(colour =  "black")) +
+    guides(fill = guide_colorbar(title = paste0(summarize_by ,"\n Loewe score")))
   
-  if (!is.list(data)) {
-    stop("Input data is not a list format!")
-  }
-  scores <- data$scores
-  drug.pairs <- data$drug.pairs
-  num.pairs <- 1:length(scores)
-  plots <- list()
-  if (!is.null(pair.index)) {
-    num.pairs <- pair.index
-  }
-  for (i in num.pairs) {
-    scores.dose <- scores[[i]]
-    drug.col <- drug.pairs$drug.col[i]
-    drug.row <- drug.pairs$drug.row[i]
-    scores.tmp <- scores.dose
-    if (!is.null(col.range)) {
-      if (col.range[1] == 1) {
-        scores.tmp <- scores.tmp[, (col.range[1] + 1):col.range[2]]
-      }
-      else {
-        scores.tmp <- scores.tmp[, col.range[1]:col.range[2]]
-      }
-    }
-    else {
-      scores.tmp <- scores.tmp[, -1]
-    }
-    if (!is.null(row.range)) {
-      if (row.range[1] == 1) {
-        scores.tmp <- scores.tmp[(row.range[1] + 1):row.range[2], 
-                                 ]
-      }
-      else {
-        scores.tmp <- scores.tmp[row.range[1]:row.range[2], 
-                                 ]
-      }
-    }
-    else {
-      scores.tmp <- scores.tmp[-1, ]
-    }
-    summary.score <- round(mean(scores.tmp, na.rm = TRUE), 
-                           3)
-    row.conc <- as.numeric(rownames(scores.dose))
-    col.conc <- as.numeric(colnames(scores.dose))
-    nr <- nrow(scores.dose)
-    nc <- ncol(scores.dose)
-    scores.extended <- .ExtendedScores(scores.dose, len)
-    mat.tmp <- scores.extended
-    if (!is.null(col.range)) {
-      mat.tmp[, which(pred.cory >= col.range[1] & pred.cory <= 
-                        col.range[2])]
-    }
-    if (!is.null(row.range)) {
-      mat.tmp[which(pred.corx >= row.range[1] & pred.corx <= 
-                      row.range[2]), ]
-    }
-    pp <- matrix(NA, nrow(mat.tmp) * ncol(mat.tmp), 3)
-    pp <- data.frame(pp)
-    colnames(pp) <- c("x", "y", "z")
-    pp$x <- rep(colnames(mat.tmp), each = nrow(mat.tmp))
-    pp$y <- rep(rownames(mat.tmp), ncol(mat.tmp))
-    pp$z <- c(mat.tmp)
-    plot.title <- paste(pat,": Average synergy: ", summary.score, 
-                        " (", data$method, ")", sep = "")
-    conc.runit <- drug.pairs$concRUnit[i]
-    conc.cunit <- drug.pairs$concCUnit[i]
-    unit.rtext <- paste("(", conc.runit, ")", sep = "")
-    unit.ctext <- paste("(", conc.cunit, ")", sep = "")
-    if(is.null(file.name)) {
-    file.name <- paste(drug.row, drug.col, "synergy", drug.pairs$blockIDs[i], 
-                       data$method, "pdf", sep = ".")
-    }
-    drug.row <- paste(drug.row, unit.rtext, sep = " ")
-    drug.col <- paste(drug.col, unit.ctext, sep = " ")
-    max.dose <- max(abs(max(scores.dose)), abs(min(scores.dose)))
-    color.range <- round(max.dose + 5, -1)
-    if (is.null(legend.start)) {
-      start.point <- -color.range
-    }
-    else {
-      start.point <- legend.start
-    }
-    if (is.null(legend.end)) {
-      end.point <- color.range
-    }
-    else {
-      end.point <- legend.end
-    }
-    levels <- seq(start.point, end.point, by = 2)
-    col1 <- colorRampPalette(c("green", "#FFFFFF"))(length(which(levels <= 
-                                                                   0)))
-    col2 <- colorRampPalette(c("#FFFFFF", "red"))(length(which(levels >= 
-                                                                 0)))
-    col <- c(col1, col2[-1])
-    if (type == "3D") {
-      if (!is.null(col.range)) {
-        xaxis <- list(at = seq(1, ncol(mat.tmp), by = len + 
-                                 1), labels = round(col.conc[col.range[1]:col.range[2]], 
-                                                    3))
-      }
-      else {
-        xaxis <- list(at = seq(1, ncol(mat.tmp), by = len + 
-                                 1), labels = round(col.conc, 3))
-      }
-      if (!is.null(row.range)) {
-        yaxis <- list(at = seq(1, nrow(mat.tmp), by = len + 
-                                 1), labels = round(row.conc[row.range[1]:row.range[2]], 
-                                                    3))
-      }
-      else {
-        yaxis <- list(at = seq(1, nrow(mat.tmp), by = len + 
-                                 1), labels = round(row.conc, 3))
-      }
-      par1 <- list(arrows = FALSE, distance = c(0.8, 0.8, 
-                                                0.8), col = 1, cex = 0.8, z = list(tick.number = 6), 
-                   x = xaxis, y = yaxis)
-      zlabs <- list(expression("Synergy score"), rot = 90, 
-                    cex = 1, axis.key.padding = 0)
-      xpar <- list(as.character(drug.col), cex = 1, rot = 20)
-      ypar <- list(as.character(drug.row), cex = 1, rot = -50)
-      fig <- wireframe(t(mat.tmp), scales = par1, drape = TRUE, 
-                       colorkey = list(space = "top", width = 0.5), 
-                       screen = list(z = 30, x = -55), zlab = zlabs, 
-                       xlab = xpar, ylab = ypar, zlim = c(start.point, 
-                                                          end.point), col.regions = col, main = plot.title, 
-                       at = do.breaks(c(start.point, end.point), length(col)), 
-                       par.settings = list(axis.line = list(col = "transparent")), 
-                       zoom = 1, aspect = 1)
-      print(fig)
-      fig <- recordPlot()
-    }
-    else if (type == "2D") {
-      dev.new(noRStudioGD = TRUE)
-      layout(matrix(c(1, 2), nrow = 2L, ncol = 1L), heights = c(0.1, 
-                                                                1))
-      par(mar = c(0, 6.1, 2.1, 4.1))
-      suppressWarnings(par(mgp = c(3, -1.5, 0)))
-      plot.new()
-      plot.window(ylim = c(0, 1), xlim = range(levels), 
-                  xaxs = "i", yaxs = "i")
-      rect(levels[-length(levels)], 0, levels[-1L], 0.3, 
-           col = col, border = NA)
-      axis(3, tick = FALSE, at = do.breaks(c(start.point, 
-                                             end.point), end.point/10))
-      title(plot.title)
-      par(mar = c(5.1, 4.1, 1.1, 2.1))
-      suppressWarnings(par(mgp = c(2, 1, 0)))
-      plot.new()
-      mat.tmp <- t(mat.tmp)
-      x.2D <- (1:dim(mat.tmp)[1] - 1)/(dim(mat.tmp)[1] - 
-                                         1)
-      y.2D <- (1:dim(mat.tmp)[2] - 1)/(dim(mat.tmp)[2] - 
-                                         1)
-      plot.window(asp = NA, xlim = range(x.2D), ylim = range(y.2D), 
-                  "", xaxs = "i", yaxs = "i")
-      .filled.contour(x.2D, y.2D, z = mat.tmp, levels, 
-                      col = col)
-      box()
-      mtext(drug.col, 1, cex = 1, padj = 3)
-      mtext(drug.row, 2, cex = 1, las = 3, padj = -3)
-      if (!is.null(row.range)) {
-        yconc <- round(row.conc[row.range[1]:row.range[2]], 
-                       3)
-      }
-      else {
-        yconc <- round(row.conc, 3)
-      }
-      if (!is.null(col.range)) {
-        xconc <- round(col.conc[col.range[1]:col.range[2]], 
-                       3)
-      }
-      else {
-        xconc <- round(col.conc, 3)
-      }
-      axis(side = 1, at = seq(0, 1, by = 1/(length(xconc) - 
-                                              1)), labels = xconc)
-      axis(side = 2, at = seq(0, 1, by = 1/(length(yconc) - 
-                                              1)), labels = yconc)
-      fig <- recordPlot()
-      dev.off()
-    }
-    else {
-      if (!is.null(col.range)) {
-        xaxis <- list(at = seq(1, ncol(mat.tmp), by = len + 
-                                 1), labels = round(col.conc[col.range[1]:col.range[2]], 
-                                                    3))
-      }
-      else {
-        xaxis <- list(at = seq(1, ncol(mat.tmp), by = len + 
-                                 1), labels = round(col.conc, 3))
-      }
-      if (!is.null(row.range)) {
-        yaxis <- list(at = seq(1, nrow(mat.tmp), by = len + 
-                                 1), labels = round(row.conc[row.range[1]:row.range[2]], 
-                                                    3))
-      }
-      else {
-        yaxis <- list(at = seq(1, nrow(mat.tmp), by = len + 
-                                 1), labels = round(row.conc, 3))
-      }
-      par1 <- list(arrows = FALSE, distance = c(0.8, 0.8, 
-                                                0.8), col = 1, cex = 0.8, z = list(tick.number = 6), 
-                   x = xaxis, y = yaxis)
-      zlabs <- list(expression("Synergy score"), rot = 90, 
-                    cex = 1, axis.key.padding = 0)
-      xpar <- list(as.character(drug.col), cex = 1, rot = 20)
-      ypar <- list(as.character(drug.row), cex = 1, rot = -50)
-      syn.3d.plot <- wireframe(t(mat.tmp), scales = par1, 
-                               drape = TRUE, colorkey = list(space = "top", 
-                                                             width = 0.5), screen = list(z = 30, x = -55), 
-                               zlab = zlabs, xlab = xpar, ylab = ypar, zlim = c(start.point, 
-                                                                                end.point), col.regions = col, main = plot.title, 
-                               at = do.breaks(c(start.point, end.point), length(col)), 
-                               par.settings = list(axis.line = list(col = "transparent")), 
-                               zoom = 1, aspect = 1)
-      layout(matrix(c(1, 2, 3, 3), nrow = 2L, ncol = 2L), 
-             heights = c(0.1, 1))
-      par(mar = c(0, 6.1, 2.1, 4.1))
-      suppressWarnings(par(mgp = c(3, -0.8, 0)))
-      plot.new()
-      plot.window(ylim = c(0, 1), xlim = range(levels), 
-                  xaxs = "i", yaxs = "i")
-      rect(levels[-length(levels)], 0, levels[-1L], 0.3, 
-           col = col, border = NA)
-      axis(3, tick = FALSE, at = do.breaks(c(start.point, 
-                                             end.point), end.point/10))
-      title(plot.title)
-      par(mar = c(5.1, 4.1, 1.1, 2.1))
-      suppressWarnings(par(mgp = c(2, 1, 0)))
-      plot.new()
-      mat.tmp <- t(mat.tmp)
-      x.2D <- (1:dim(mat.tmp)[1] - 1)/(dim(mat.tmp)[1] - 
-                                         1)
-      y.2D <- (1:dim(mat.tmp)[2] - 1)/(dim(mat.tmp)[2] - 
-                                         1)
-      plot.window(asp = NA, xlim = range(x.2D), ylim = range(y.2D), 
-                  "", xaxs = "i", yaxs = "i")
-      .filled.contour(x.2D, y.2D, z = mat.tmp, levels, 
-                      col = col)
-      box()
-      mtext(drug.col, 1, cex = 1, padj = 3)
-      mtext(drug.row, 2, cex = 1, las = 3, padj = -3)
-      if (!is.null(row.range)) {
-        yconc <- round(row.conc[row.range[1]:row.range[2]], 
-                       3)
-      }
-      else {
-        yconc <- round(row.conc, 3)
-      }
-      if (!is.null(col.range)) {
-        xconc <- round(col.conc[col.range[1]:col.range[2]], 
-                       3)
-      }
-      else {
-        xconc <- round(col.conc, 3)
-      }
-      axis(side = 1, at = seq(0, 1, by = 1/(length(xconc) - 
-                                              1)), labels = xconc)
-      axis(side = 2, at = seq(0, 1, by = 1/(length(yconc) - 
-                                              1)), labels = yconc)
-      print(syn.3d.plot, position = c(0.5, 0, 1, 1), newpage = FALSE)
-      fig <- recordPlot()
-    }
-    plots[[i]] <- fig
-    if (save.file) {
-      if (type %in% c("2D", "3D")) {
-        pdf(file.name, width = 10, height = 10)
-      }
-      else {
-        pdf(file.name, width = 12, height = 6)
-      }
-      print(fig)
-      dev.off()
-    }
-  }
-  if (!save.file) {
-    for (i in num.pairs) {
-      dev.new(noRStudioGD = TRUE)
-      replayPlot(plots[[i]])
-    }
-  }
+  
+  df10x10_dr<- filter(df10x10, BaseDrugName == dr) %>% group_by(concBvalue, concCvalue) %>% summarize(sumViab = switch(summarize_by,
+                                                                                                                       mean  = mean(normalizedValue),
+                                                                                                                       median  = median(normalizedValue)))
+  gg_medViab <- ggplot(df10x10_dr, aes(x = factor(round(concBvalue*1000,2)), y=factor(round(concCvalue*1000,2)), fill = sumViab)) +
+    geom_tile() +
+    ylab("Ibrutinib (nM)") + xlab(paste(dr, "(nM)")) +
+    scale_fill_gradient(low = "white",high = "navy", limits=c(0,1.4)) +
+    theme_bw(base_size = 16) + coord_fixed() +
+    theme(axis.text.x = element_text(angle=90, vjust=1, hjust=1),
+          plot.title = element_text(colour =  "black")) +
+    guides(fill = guide_colorbar(title = paste0(summarize_by ,"\n viability")))
+  
+  cowplot::plot_grid(gg_medScore, gg_medViab, nrow=2, align = "v", axis ="lr")
 }
--- runAll.R
+++ runAll.R
@@ -1,11 +1,11 @@
 # start from a new session, requires connection to the server
 library(knitr)
 rm(list=ls())
-today <- "180905"
+today <- "180914"
 rmarkdown::render("~/Documents/cll/MarinaDrugComb/DrugCombi_Code4Ms/DrugCombi_DataImport.Rmd", params = list(today=today))
 rmarkdown::render("~/Documents/cll/MarinaDrugComb/DrugCombi_Code4Ms/DrugCombi_QC.Rmd", params = list(today=today))
 rmarkdown::render("~/Documents/cll/MarinaDrugComb/DrugCombi_Code4Ms/DrugCombi_AddOmicsData.Rmd", params = list(today=today))
 rmarkdown::render("~/Documents/cll/MarinaDrugComb/DrugCombi_Code4Ms/DrugCombi_BaseAlone.Rmd", params = list(today=today))
 rmarkdown::render("~/Documents/cll/MarinaDrugComb/DrugCombi_Code4Ms/DrugCombi_Combi.Rmd", params = list(today=today))
-rmarkdown::render("~/Documents/cll/MarinaDrugComb/DrugCombi_Code4Ms/DrugCombi_CombiSynergy.Rmd", params = list(today=today))
 rmarkdown::render("~/Documents/cll/MarinaDrugComb/DrugCombi_Code4Ms/DrugCombi_10x10.Rmd", params = list(today=today))
+rmarkdown::render("~/Documents/cll/MarinaDrugComb/DrugCombi_Code4Ms/DrugCombi_CombiSynergy.Rmd", params = list(today=today))
