dc38e97e9fec9f731b4e93120f91d12cdc762ea4
Britta Velten <britta.velten@embl.de>
2018-10-12 10:52:53 +0200

    minor change

--- runLoeweExpl.R
+++ runLoeweExpl.R
@@ -1,6 +1,6 @@
 load("out/df10x10_180914.Rdata")
 for(drtmp in c("Afatinib", "Navitoclax", "Idelalisib", "Venetoclax")){
-  for(pattmp in unique(filter(df10x10, BaseDrugName == dr)$PatientID)){
+  for(pattmp in unique(filter(df10x10, BaseDrugName == drtmp)$PatientID)){
     rmarkdown::render("Exploration_Loewe_ZIP.Rmd",
                       params = list(dr=drtmp, pat=pattmp),
                       output_file = paste0(paste(drtmp,pattmp,sep="_"),".html"))
