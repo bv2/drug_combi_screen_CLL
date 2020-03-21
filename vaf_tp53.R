261c44a47f85ef20e5b2438c4e6f219e416e5a2e
bv2 <britta.velten@gmail.com>
2020-03-21 16:11:30 +0100

    minor changes and  cleaning

--- vaf_tp53.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-vaf <- read.csv("~/Google Drive/CombiScreen/revision/add_info/TP53_VAF.csv", sep = ";", dec = ",")
-minvaf <- min(vaf$VAF, na.rm = T)
-
-pdf("~/Google Drive/CombiScreen/revision/add_info/VAF.pdf", height = 4)
-vaf %>% mutate(VAF = as.numeric(VAF)) %>% ggplot(aes(x=VAF)) +
-    geom_histogram(fill = "navy", bin =100) + theme_bw() + geom_vline(xintercept = minvaf, col = "red", lty = 2) +
-    ylab("Number of samples") + xlab("TP53 variant allele frequency (%)")
-dev.off()
